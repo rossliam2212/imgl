7afc470fbbe03577ada8a5d2b669ac847d75437e
rossliam2212 <rossliam2212@gmail.com>
2025-01-30 20:52:43 +0000

    Added Defines.hpp

---
+++ include/imgl/Defines.hpp
@@ -0,0 +1,20 @@
+/**
+ * @file Defines.hpp
+ * @author liam 
+ * @date 30/01/2025
+ */
+
+#ifndef DEFINES_HPP
+#define DEFINES_HPP
+
+#include <string>
+
+namespace imgl {
+	inline const char* FILTER_CMD{"filter"};
+	inline const char* FILTER_OPTION_TYPE{"type"};
+	inline const char* FILTER_OPTION_INPUT{"input"};
+	inline const char* FILTER_OPTION_OUTPUT{"-o,--output"};
+	inline const char* FILTER_OPTION_INTENSITY{"--intensity"};
+} // namespace imgl
+
+#endif //DEFINES_HPP
