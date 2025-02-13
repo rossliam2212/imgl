c27427cccc3b2d0460d1018321549cd634841cb7
rossliam2212 <rossliam2212@gmail.com>
2025-02-13 23:00:43 +0000

    Updated Defines.hpp

--- include/imgl/Defines.hpp
+++ include/imgl/Defines.hpp
@@ -16,6 +16,12 @@ namespace imgl {
 	inline const char* FILTER_OPTION_OUTPUT{"-o,--output"};
 	inline const char* FILTER_OPTION_INTENSITY{"--intensity"};
 
+	const std::string FILTER_TYPE_SHARPEN{"sharpen"};
+	const std::string FILTER_TYPE_BLUR{"blur"};
+	const std::string FILTER_TYPE_GRAYSCALE{"grayscale"};
+	constexpr float FILTER_OPTION_INTENSITY_MIN{0.1f};
+	constexpr float FILTER_OPTION_INTENSITY_MAX{2.f};
+
 	inline const char* TRANSFORM_CMD{"transform"};
 	inline const char* TRANSFORM_OPTION_TYPE{"type"};
 	inline const char* TRANSFORM_OPTION_INPUT{"input"};
@@ -23,6 +29,14 @@ namespace imgl {
 	inline const char* TRANSFORM_OPTION_ANGLE{"--angle"};
 	inline const char* TRANSFORM_OPTION_WIDTH{"--width"};
 	inline const char* TRANSFORM_OPTION_HEIGHT{"--height"};
+
+	inline const char* DEFAULT_VERT_SHADER{"../../../resources/shaders/default_vertex.glsl"};
+	inline const char* SHARPEN_FRAG_SHADER{"../../../resources/shaders/sharpen_fragment.glsl"};
+
+	inline const char* EXTENSION_PNG{".png"};
+	inline const char* EXTENSION_JPG{".jpg"};
+	inline const char* EXTENSION_JPEG{".jpEg"};
+	inline const char* EXTENSION_BMP{".bmp"};
 } // namespace imgl
 
 #endif //DEFINES_HPP
