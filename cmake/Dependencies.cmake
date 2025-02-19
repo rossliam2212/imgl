a83a5ac58c355784165cd0e8ad0388192df35b9e
rossliam2212 <rossliam2212@gmail.com>
2025-02-19 21:59:37 +0000

    Removed glm dep

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -6,7 +6,6 @@ function(setup_dependencies)
     find_package(OpenGL REQUIRED)
     find_package(glfw3 REQUIRED)
     find_package(GLEW REQUIRED)
-    find_package(glm REQUIRED)
     find_package(spdlog REQUIRED)
     find_package(CLI11 REQUIRED)
 
